7f3083e18592c9425a265c82b7f6f9a9902b4b17
Kevin Burkett <stlbucket@gmail.com>
2024-05-11 18:45:41 -0700

    DemoInfo

---
+++ components/_common/DemoInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-between bg-red-600 p-2 rounded">
+    <p>This is a prototype instance - all data is subject to removal at any time.</p>
+    <a class="text-blue-900 underline" v-if="isDemoMode" href="http://localhost:54324/monitor">Inbucket Monitor</a>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const isDemoMode = computed(() => {
+  return useRuntimeConfig().public.DEMO_MODE
+})
+</script>
