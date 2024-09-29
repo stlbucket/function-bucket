ddee774e51bcf5f1e53f32604045db232f4a5453
Kevin Burkett <stlbucket@gmail.com>
2024-09-29 16:00:08 -0700

    user home page

--- components/_homepage/HomePageDefault.vue
+++ components/_homepage/HomePageDefault.vue
@@ -1,5 +1,13 @@
 <template>
-  DEFAULT HOME PAGE
+  <UCard>
+    <template #header>
+      <div class="flex justify-center text-2xl">DEFAULT USER HOME PAGE</div>
+    </template>
+    <div class="flex flex-col gap-3">
+      <div class="flex justify-center">This is the default home page for the base application</div>
+      <div class="flex justify-center">Only users of the anchor tenant should ever see this homepage, and only when they are working in the base context.</div>
+    </div>
+  </UCard>
 </template>
 
 <script lang="ts" setup></script>
